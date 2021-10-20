cc4cfbda832019dd667b34ed6e11423a9cee9117
LimitR <kakashihotakimail.ru@gmail.com>
2021-10-21 00:38:03 +0300

    paint win

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,117 @@
-pub(crate) use std::{fs::File, io::stdin, io::stdout, io::Write};
+pub(crate) use std::{fs::File, io::stdin};
+use std::io::{stdout, Write};
 use crossterm::QueueableCommand;
 use crossterm::event::{MouseEvent, MouseEventKind, poll};
+use crossterm::style::Colorize;
 use crossterm::{
-    cursor::{DisableBlinking, EnableBlinking, MoveTo, RestorePosition, SavePosition, position},
-    event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent},
+    terminal,
+    queue,
     execute,
+    style,
+    cursor::{DisableBlinking, EnableBlinking, MoveTo, RestorePosition, SavePosition, position},
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent},
     terminal::{disable_raw_mode, enable_raw_mode},
     Result,
+    cursor
 };
-use serde::Deserialize;
 use std::time::Duration;
 //cd Desktop\rust_pet_project\pet_project
 fn main()  -> Result<()> {
-    let mut x: Vec<String> = Vec::new();
-    let mut final_vector: Vec<&str> = Vec::new();
-    let mut out_value = String::new();
     let stdin = stdin();
+    execute!(stdout(), EnableMouseCapture)?;
+    for x in 0..150{
+        println!(" ");
+    }
     let mut stdout = stdout();
-    execute!(stdout, EnableMouseCapture)?;
-    loop{
-        let event = read()?;
-        // let testing = Event::Key(KeyCode::Char('c'));
+    let mut kist = "●".white();
+    let mut color_cursor = style::PrintStyledContent( kist);   
+    let mut position_cursor: (u16, u16) = (0, 0);
+
+    loop{     
         
-        if let Event::Key(KeyEvent {
-            code: KeyCode::Char('c'),
-            ..
-        }) = event{
-            println!("_______");
+        let event_win = read()?;
+
+        if let Event::Key(KeyEvent { code: KeyCode::Char('0'), .. }) = event_win{
+            color_cursor = style::PrintStyledContent( kist.white());
+        }
+        if let Event::Key(KeyEvent { code: KeyCode::Char('1'), .. }) = event_win{
+            color_cursor = style::PrintStyledContent( kist.red());
+        }
+        if let Event::Key(KeyEvent { code: KeyCode::Char('2'), .. }) = event_win{
+            color_cursor = style::PrintStyledContent( kist.blue());
+        }
+        if let Event::Key(KeyEvent { code: KeyCode::Char('3'), .. }) = event_win{
+            color_cursor = style::PrintStyledContent( kist.magenta());
+        }
+        if let Event::Key(KeyEvent { code: KeyCode::Char('4'), .. }) = event_win{
+            color_cursor = style::PrintStyledContent( kist.black());
         }
 
         if let Event::Mouse(MouseEvent {
-            column: 64,
-            row: 13,
+        kind: MouseEventKind::Drag(_),
+            column: x,
+            row: y,
             ..
-        }) = event{
-            println!("____MOUSES___");
+        }) = event_win {
+
+                queue!(stdout, cursor::MoveTo(x,y), color_cursor)?;
         }
 
-        println!("{:?}", event);
+
+        if let Event::Mouse(MouseEvent { 
+            kind: MouseEventKind::Down(Left), 
+            column: x, 
+            row: y,  
+            .. 
+        }) = event_win {
+
+            queue!(stdout, cursor::MoveTo(x,y), color_cursor)?;
+        }
+        if let Event::Mouse(MouseEvent { 
+            kind: MouseEventKind::Up(Left), 
+            column: x, 
+            row: y,
+            .. 
+        }) = event_win {
+            queue!(stdout, cursor::MoveTo(x, y), color_cursor)?;
+        }
         
-        // if event == Event::Mouse(MouseEvent){
-        //     println!("TESTESTESTSET");
-        // }
-        
-        if event == Event::Key(KeyCode::Esc.into()) {
-            
+   
+        if event_win == Event::Key(KeyCode::Esc.into()) {
             break;
         }
     }
-    // loop {
-    //     stdin.read_line(&mut out_value).expect("ERROR");
-    //     if out_value.trim() == "ok".to_string() {
-    //         let mut file = File::create("foo.txt").expect("ERROR CREATE FILE");
-    //         for value in &x {
-    //             final_vector.push(&value[0..value.len() - 2]);
-    //             file.write(value.as_bytes()).unwrap();
-    //         }
-    //         stdout()
-    //                 .execute(SetForegroundColor(Color::Red))?
-    //                 .execute(SetBackgroundColor(Color::Green))?
-    //                 .execute(Print(&final_vector.join(" | ")))?
-    //                 .execute(ResetColor)?;
-    //         break;
-    //     };
-    //     x.push(out_value);
-    //     out_value = "".to_string();
-    // }
-    // out_value = "".to_string();
-    // loop{
-    //     stdin.read_line(&mut out_value).expect("ERROR");
-    //     if out_value.trim() == "end".to_string() {
-    //         break;
-    //     }
-    // }
+
     Ok(())
 }
+
+/*
+            // if x > position_cursor.0 && y ==  position_cursor.1{
+            //     kist = "―"
+            // }else if x < position_cursor.0 && y ==  position_cursor.1{
+            //     kist = "―"
+            // }
+
+            // if x == position_cursor.0 && y <  position_cursor.1{
+            //     kist = "│"
+            // }else if x == position_cursor.0 && y >  position_cursor.1{
+            //     kist = "│"
+            // }
+
+
+
+
+            // if x < position_cursor.0 && y <  position_cursor.1{
+            //     kist = "╱"
+            // }else if x > position_cursor.0 && y >  position_cursor.1{
+            //     kist = "╱"
+            // }
+
+            // if x > position_cursor.0 && y >  position_cursor.1{
+            //     kist = "╲"
+            // }else if x < position_cursor.0 && y <  position_cursor.1{
+            //     kist = "╲"
+            // }
+
+*/
