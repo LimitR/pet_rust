da67094833a60f483a829ab863359d34c461a25d
LimitR <kakashihotakimail.ru@gmail.com>
2022-02-05 14:51:30 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,28 @@
 pub(crate) use std::{fs::File, io::stdin};
+use std::io;
 use std::io::{stdout, Write};
-use crossterm::QueueableCommand;
-use crossterm::event::{MouseEvent, MouseEventKind, poll, MouseButton};
+use crossterm::event::{MouseEvent, MouseEventKind, MouseButton};
 use crossterm::style::Colorize;
 use crossterm::{
-    terminal,
     queue,
     execute,
     style,
-    cursor::{DisableBlinking, EnableBlinking, MoveTo, RestorePosition, SavePosition, position},
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent},
-    terminal::{disable_raw_mode, enable_raw_mode},
+    event::{read, EnableMouseCapture, Event, KeyCode, KeyEvent},
     Result,
     cursor
 };
-use std::time::Duration;
-//cd Desktop\rust_pet_project\pet_project
 
-
-const MENU: &str = "red
-blue
-black";
 
 
 fn main()  -> Result<()> {
-    let stdin = stdin();
     execute!(stdout(), EnableMouseCapture)?;
-    for x in 0..150{
-        println!(" ");
-    }
+
     let mut stdout = stdout();
-    let mut kist = "●".white();
-    let mut color_cursor = style::PrintStyledContent( kist);   
-    let mut position_cursor: (u16, u16) = (0, 0);
-    let mut position_cursor_y: u16 = 0;
+    let kist = "●".white();
+    let mut color_cursor = style::PrintStyledContent( kist);
 
-    // for line in MENU.split('\n') {
-    //     queue!(stdout, style::Print(line), cursor::MoveToNextLine(1))?;
-    // };
+    loop{
 
-    /*
-    printin!(stdout, cursor::MoveTo(1, 30), style::PrintStyledContent( "Red".red()))?;
-    queue!(stdout, cursor::MoveTo(10,30), style::PrintStyledContent( "White".white()))?;
-    queue!(stdout, cursor::MoveTo(20,30), style::PrintStyledContent( "Blue".blue()))?;
-    queue!(stdout, cursor::MoveTo(30,30), style::PrintStyledContent( "Black".black()))?;
-     */
-    loop{     
-        
         let event_win = read()?;
 
         if let Event::Key(KeyEvent { code: KeyCode::Char('0'), .. }) = event_win{
@@ -65,6 +40,12 @@ fn main()  -> Result<()> {
         if let Event::Key(KeyEvent { code: KeyCode::Char('4'), .. }) = event_win{
             color_cursor = style::PrintStyledContent( kist.black());
         }
+        if let Event::Key(KeyEvent { code: KeyCode::Enter, ..}) = event_win{
+            let mut result = File::create("./pic.txt").unwrap();
+            let mut text = String::from("");
+            io::stdin().read_line(&mut text).unwrap();
+            result.write_all(text.clone().as_bytes()).unwrap();
+        }
 
         if let Event::Mouse(MouseEvent {
         kind: MouseEventKind::Drag(MouseButton::Left),
@@ -74,94 +55,51 @@ fn main()  -> Result<()> {
         }) = event_win {
 
                 queue!(stdout, cursor::MoveTo(x,y), color_cursor)?;
-        }
 
+        }
         if let Event::Mouse(MouseEvent {
             kind: MouseEventKind::Drag(MouseButton::Right),
                 column: x,
                 row: y,
                 ..
             }) = event_win {
-    
+
                     queue!(stdout, cursor::MoveTo(x,y), style::PrintStyledContent( kist.black()))?;
             }
 
-
-        if let Event::Mouse(MouseEvent { 
-            kind: MouseEventKind::Down(MouseButton::Left), 
-            column: x, 
-            row: y,  
-            .. 
+        if let Event::Mouse(MouseEvent {
+            kind: MouseEventKind::Down(MouseButton::Left),
+            column: x,
+            row: y,
+            ..
         }) = event_win {
 
             queue!(stdout, cursor::MoveTo(x,y), color_cursor)?;
         }
-        if let Event::Mouse(MouseEvent { 
-            kind: MouseEventKind::Up(MouseButton::Left), 
-            column: x, 
+        if let Event::Mouse(MouseEvent {
+            kind: MouseEventKind::Up(MouseButton::Left),
+            column: x,
             row: y,
-            .. 
+            ..
         }) = event_win {
             queue!(stdout, cursor::MoveTo(x, y), color_cursor)?;
         }
 
-
-
-            if let Event::Resize(x, y) = event_win{
-                // println!("x - {}\ny - {}\n", x , y);
-                position_cursor.0 = x;
-                position_cursor.1 = y;
-                position_cursor_y = y;
-                queue!(stdout, cursor::MoveTo(0,y), style::PrintStyledContent( "TEST".white()))?;
-                queue!(stdout, cursor::MoveTo(10,y), style::PrintStyledContent( "TEST_2".white()))?;
-                queue!(stdout, cursor::MoveTo(20,y), style::PrintStyledContent( "TEST_3".white()))?;
-            }
-
-            if let Event::Mouse(MouseEvent { 
-                kind: MouseEventKind::Down(MouseButton::Left), 
-                column: 0, 
-                row: position_cursor_y,  
-                .. 
+            if let Event::Mouse(MouseEvent {
+                kind: MouseEventKind::Down(MouseButton::Left),
+                column: 0,
+                ..
             }) = event_win {
-    
+
                 color_cursor = style::PrintStyledContent( kist.red());
             }
-        
-   
+
+
         if event_win == Event::Key(KeyCode::Esc.into()) {
             break;
         }
+        stdout.flush().unwrap();
     }
 
     Ok(())
 }
-
-/*
-            // if x > position_cursor.0 && y ==  position_cursor.1{
-            //     kist = "―"
-            // }else if x < position_cursor.0 && y ==  position_cursor.1{
-            //     kist = "―"
-            // }
-
-            // if x == position_cursor.0 && y <  position_cursor.1{
-            //     kist = "│"
-            // }else if x == position_cursor.0 && y >  position_cursor.1{
-            //     kist = "│"
-            // }
-
-
-
-
-            // if x < position_cursor.0 && y <  position_cursor.1{
-            //     kist = "╱"
-            // }else if x > position_cursor.0 && y >  position_cursor.1{
-            //     kist = "╱"
-            // }
-
-            // if x > position_cursor.0 && y >  position_cursor.1{
-            //     kist = "╲"
-            // }else if x < position_cursor.0 && y <  position_cursor.1{
-            //     kist = "╲"
-            // }
-
-*/
